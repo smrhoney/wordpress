f007defd9f958084fca38690299e0d83ee111781
Shawn Rhoney <smrhoney@gmail.com>
2012-12-01 20:03:16 -0600

    plugin-loader initial commit

---
+++ wp-content/plugins/vimeo-gallery/plugin-loader.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Vimeo Gallery
+ * Plugin URI: http://somedomain.com
+ * Description: Shows a Gallery of Videos from a Vimeo Account
+ * Version: 0.5
+ * Author: Shawn Rhoney
+ * Author URI: http://somedomain.com
+ */
+?>
+
